97a40b033ef752bd682aacc2ed3656e9a8327c43
Chen Gang-9T <fouryu@sina.com.cn>
2012-06-18 18:13:24 +0800

    update sprint.py

--- sprint.py
+++ sprint.py
@@ -1,199 +1,174 @@
 #! /usr/bin/env python
-
 import os
-from os import path
-
-SprintPrefix="Sprint-"
-SprintDocList=["sprint_backlog", "sprint_review"]
-
-def get_last_num():
-    '''Return the number of the last sprint in current dir'''
-    flist=os.listdir(".")
-    msi = 0
-    for f in flist:
-        if f.rfind(SprintPrefix) == 0:
-            if int(f.split('-')[1]) > msi: msi = int(f.split('-')[1])
-    return msi
-
-    
-def create_folder_if_not_exist(folder):
-    '''Create folder in case the folder not exist in cur dir'''
-    if path.isdir(folder):
-        return
-    else:
-        os.mkdir(folder)
-
-def rmsubsys(folder='.'):
-    '''remove a directory
-    
-    It will work even the folder not empty
-    '''
-    os.chdir(folder)
-    for f in os.listdir('.'):
-        if os.path.isdir(f): rmsubsys(f)
-        else: os.remove(f)
-    if folder == '.': folder=os.path.basename(os.getcwd())
-    os.chdir('..')
-    os.rmdir(folder)
-
-    
-class status:
-    available_status=['new', 'ongoing', 'done', 'undefined']
-    def __init__(self, st='undefined'):
-        self.val='undefined'
-        self.update(st)
-        
-    def update(self, st):
-        if st in self.available_status: self.val = st
-        
-    def show(self):
-        return self.val
-    
-class sprint:
-    def __init__(self, st='undefined'):
-        self.status = status(st)
-
-    def state(self, newstate=''):
-        if newstate == '': return self.status.show()
-        else: self.status.update(newstate)    
-        
-
-class sprintDoc(sprint):
-    '''Define the structure of the sprint documents
-    
-    sprint documents include sprint_backlog and sprint_review
-    '''
-    def __init__(self, name="", itemkeys=[]):
-        sprint.__init__(self)
-        self.name = name
-        self.keys = []
-        self.items = {'others': []}
-        self.init_items(itemkeys)
-        
-    def init_items(self, itemkeys): 
-        self.keys = itemkeys
-        for key in itemkeys: self.items[key] = []
-
-    def check(self):
-        '''To check the integrity of the document'''
-        if self.read() == 0: 
-            for key in self.keys:
-                print "content check for " + key + " is " + str(self.__content_error(key))
-                if self.__content_error(key): self.state('undefined')
-        else: 
-            self.write()
-            self.state('new')
-        return self.state()
-
-    def read(self):
-        '''read the items out'''
-        try:
-            f = open(self.name, 'r+')
-        except IOError:
-            return -1
-        for key in self.items.keys(): self.items[key] = []
-        cur_key = 'others'
-        for line in f:
-            for key in self.keys: 
-                if line == key + ":\n": 
-                    cur_key = key
-                    print "find key: " + key
-                else: self.items[cur_key].append(line)
-        f.close()
-        return 0
-    
-    def write(self):
-        '''write the items back'''
-        f = open(self.name, 'w+')
-        for key in self.keys: 
-            f.write(key + ':\n')
-            print "content check for " + key + " is " + str(self.__content_error(key))
-            if self.__content_error(key): self.items[key].append('')
-            f.writelines(self.items[key])
-        f.close()
-
-    def __content_error(self, key):
-        return (len(self.items[key]) == 0)
-
-class sprintBacklog(sprintDoc):
-    def __init__(self):
-        sprintDoc.__init__(self, "sprint_backlog", ["Commited Items", "Try Items"])
+from os import system
+import shutil
 
 
-class sprintReview(sprintDoc):
-    def __init__(self):
-        sprintDoc.__init__(self, "sprint_review", ["Achievements", "Not done items"])
-
-SprintObjDic={"sprint_backlog":sprintBacklog(), "sprint_review":sprintReview()}
-
-class sprintDir(sprint):
-    SprintDocList=["sprint_backlog", "sprint_review"]
+SprintDirPrefix='Sprint-'
+class sprintDir:
+    fileList=['sprint_backlog', 'sprint_review']
     def __init__(self, n):
-        sprint.__init__(self)
-        self.name = SprintPrefix + str(n)
-        self.docs = []
-        for doc in self.SprintDocList: self.docs.append(SprintObjDic[doc])
+        self.__name=SprintDirPrefix+str(n)
+        self.__state = 'unavailable'
+        self.__docs = []
+        
+    def getname(self):
+        return self.__name
         
     def check(self):
-        os.chdir(self.name)
-        firstState=self.docs[0].check()
-        self.state(firstState)
-        for doc in self.docs: 
-            status = doc.check()
-            if status != firstState:
-                if status != 'undefined' and firstState != 'undefined':
-                    self.state('ongoing')
-                else: 
-                    self.state('undefined')
-                    break
-        print self.name + " status is: " + self.state()
-        os.chdir('..')
+        if os.path.isdir(self.__name): 
+            self.__state='new'
+            for filename in self.fileList:
+                if filename not in os.listdir(self.__name): 
+                    self.__state='undefined'
+        return self.__state
         
-    def destroy(self):
-        if self.check() == 'new': rmsubsys(self.name)
-
     def initialize(self):
-        if path.isdir(self.name): rmsubsys(self.name)
-        os.mkdir(self.name)
-        self.check()
-         
-class sprintOp:
-    '''sprint folder operation
+        os.mkdir(self.__name)
+        os.chdir(self.__name)
+        for fname in self.fileList: 
+            f=open(fname, 'w+')
+            f.close()
+        os.chdir('..')
+        pass
 
-    It used to automatic add new sprint, check integrity of 
-    the existing sprint folder.
-    '''
-    def __init__(self, num):
-        self.num = num
-        self.sprintDir=sprintDir(0)
-        self.docs = []
-        for doc in SprintDocList: self.docs.append(SprintObjDic[doc])
-        self.name = SprintPrefix + '0'
-
-    def locate_num(self, n):
-        self.num = n
-        self.name = SprintPrefix+str(n)
+class sprintDoc:
+    def __init__(self, fname):
+        self.__name = fname
+        pass
         
     def check(self):
-        if self.num == 0:
-            print "not initialized yet"
-            return
-        create_folder_if_not_exist(self.name)
-
-        os.chdir(self.name)
-        for doc in self.docs: doc.check()
-        os.chdir("..")
+        if not os.path.isfile(self.__name): return 'unavailable'
+        if self.__name not in sprintDir.fileList: return 'undefined'
+        return 'new'
         
-    def new(self):
-        self.locate_num(get_last_num() + 1)
-        self.check()
+    def initialize(self):
+        if self.__name not in sprintDir.fileList: raise NameError(self.__name)
+        f=open(self.__name, 'w+')
+        f.close()
+
+# Test cases        
+import unittest
+
+class SprintDocTests(unittest.TestCase):
+    def setUp(self):
+        self.dirBeforeTest=os.listdir('.')
+        self.curdir=os.getcwd()
+
+    def tearDown(self):
+        self.assertEqual(self.curdir,os.getcwd(),"pwd change not allowed")
+        self.assertEqual(self.dirBeforeTest, os.listdir('.'), "files change not allowed")
+
+    def test_doc_notexist(self):
+        self.doc = sprintDoc('123')
+        self.assertEqual('unavailable', self.doc.check())
         
-    def delete(self, n):
-        self.locate_num(n)
-        rmsubsys(self.name)
+    def test_initialize_unknown_doc(self):
+        self.doc = sprintDoc('123')
+        self.assertRaises(NameError, self.doc.initialize)
 
+    def _initialize_doc_test(self, fname, filestate):
+        self.doc = sprintDoc(fname)
+        self.doc.initialize()
+        self.assertEqual(filestate, self.doc.check())
+        self.assertTrue(os.path.isfile(self.doc._sprintDoc__name))
+        os.remove(self.doc._sprintDoc__name)
 
-if __name__=="__main__":
-    sprintOp=sprintOp(0)
-    sprintOp.new()
+    def test_initialize_backlog(self):
+        self._initialize_doc_test('sprint_backlog', 'new')
+        
+    def test_initialize_review(self):
+        self._initialize_doc_test('sprint_review', 'new')
+        
+    def _exist_doc_test(self, fname, filestate):
+        self.doc = sprintDoc(fname)
+        f = open(fname, 'w+')
+        f.close()
+        self.assertEqual(filestate, self.doc.check())
+        os.remove(fname)
+    
+    def test_check_exist_unknown_doc(self):
+        self._exist_doc_test('123', 'undefined')
+        
+    def test_check_exist_backlog(self):
+        self._exist_doc_test('sprint_backlog', 'new')        
 
+class sprintDocStub:
+    def __init__(self, fname):
+        pass
+
+    def set_state(self, state):
+        pass
+        
+class SprintDirTests(unittest.TestCase):
+    def _attach_file(self, sprint, fname, state='new'):
+        # os.chdir(sprint.getname())
+        doc=sprintDocStub(fname)
+        doc.set_state('new')
+        sprint._sprintDir__docs.append(doc)
+        # os.chdir('..')
+
+    def _createTestSprint(self, dirname, status):
+        os.mkdir(dirname)
+        if status == 'CreateNew': 
+            self._attach_file(self.sprint, 'sprint_backlog')
+            f=open(dirname+'/sprint_backlog', 'w+')
+            f.close()
+            f=open(dirname+'/sprint_review', 'w+')
+            f.close()
+        elif status == 'CreateEmpty': pass
+        elif status == 'CreateWrong': 
+            self._attach_file(self.sprint, '123')
+            # f=open(dirname+'/123', 'w+')
+            # f.close()
+
+    def _initTestData(self, n, createType=''):
+        self.testIdx=n
+        self.sprint=sprintDir(self.testIdx)
+        if createType != '': self._createTestSprint(self.sprint.getname(), createType)
+
+    def setUp(self):
+        self._initTestData(0)
+        self.dirBeforeTest=os.listdir('.')
+        self.curdir=os.getcwd()
+
+    def tearDown(self):
+        if(os.path.isdir(self.sprint.getname())): shutil.rmtree(self.sprint.getname())
+        self.assertEqual(self.curdir,os.getcwd(),"pwd change not allowed")
+        self.assertEqual(self.dirBeforeTest, os.listdir('.'), "files change not allowed")
+
+    def testCheckNotexist(self):
+        self.assertEqual("unavailable",self.sprint.check())
+        
+    def _createNewCheck(self, n=0):
+        self._initTestData(n)
+        self.sprint.initialize()
+        self.assertEqual(SprintDirPrefix+str(self.testIdx), self.sprint.getname())
+        self.assertTrue(os.path.isdir(self.sprint.getname()))
+        self.assertEqual('new', self.sprint.check())
+        self.assertEqual(self.sprint.fileList, os.listdir(self.sprint.getname()))
+        
+    def testCreateNew(self):
+        self._createNewCheck()
+        
+    def testCreateAnother(self):
+        self._createNewCheck(1)
+
+    def testCheckExistNewSprint(self):
+        self._initTestData(1,'CreateNew')
+        self.assertEqual('new', self.sprint.check())
+
+    def testCheckExistEmpty(self):
+        self._initTestData(1,'CreateEmpty')
+        self.assertEqual('undefined', self.sprint.check())
+        
+    def testCheckExitWrong(self):
+        self._initTestData(1, 'CreateWrong')
+        self.assertEqual('undefined', self.sprint.check())
+        
+
+def main():
+    unittest.main()
+
+if __name__ == "__main__":
+    main()
