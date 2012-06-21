1c2dca2fb31fbf76330ab19dac80bcf264033df7
Chen Gang-Cuble <fouryu@sina.com.cn>
2012-06-21 20:36:07 +0800

    continue ut on sprint

--- src/sprint/sprint.py
+++ src/sprint/sprint.py
@@ -1,14 +1,11 @@
 #! /usr/bin/env python
 import os
-from os import system
-import shutil
 
-
-SprintDirPrefix='Sprint-'
 class sprintDir:
-    fileList=['sprint_backlog', 'sprint_review']
+    prefix = 'Sprint-'
+    fileList = ['sprint_backlog', 'sprint_review']
     def __init__(self, n):
-        self.__name=SprintDirPrefix+str(n)
+        self.__name = sprintDir.prefix + str(n)
         self.__state = 'unavailable'
         self.__docs = []
         for f in self.fileList: self.__docs.append(sprintDoc(f))
@@ -17,21 +14,23 @@ class sprintDir:
         return self.__name
         
     def check(self):
-        if os.path.isdir(self.__name): 
-            self.__state='new'
+        state = 'unavailable'
+        if os.path.isdir(self.__name):
+            os.chdir(self.__name)
+            state = 'new' 
             for doc in self.__docs:
                 if doc.check() != 'new': 
-                    self.__state = 'undefined'
+                    state = 'undefined'
                     break
-        return self.__state
+        os.chdir("..")
+        return state
         
     def initialize(self):
         os.mkdir(self.__name)
         os.chdir(self.__name)
         for doc in self.__docs:
             doc.initialize()
-        os.chdir('..')
-        pass
+#        os.chdir('..')
 
 class sprintDoc:
     def __init__(self, fname):
@@ -45,11 +44,11 @@ class sprintDoc:
         
     def initialize(self):
         if self.__name not in sprintDir.fileList: raise NameError(self.__name)
-        f=open(self.__name, 'w+')
+        f = open(self.__name, 'w+')
         f.close()
 
 def sprint_main():
     pass
 
-if __name__=='__main__':
+if __name__ == '__main__':
     sprint_main()
