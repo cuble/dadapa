ec23a738d3e74c139041453e25546665c21d9efe
Chen Gang-Cuble <fouryu@sina.com.cn>
2012-06-29 00:03:54 +0800

    update wxexample.py

--- src/backlog-editor/wxexample.py
+++ src/backlog-editor/wxexample.py
@@ -1,12 +1,32 @@
 import wx
 
-class myFrame(wx.Frame):
+class aboutBox(wx.Frame):
     def __init__(self, parent, title):
-	wx.Frame.__init__(self, parent, title=title, size=(200,100))
-        self.control = wx.TextCtrl(self, style=wx.TE_MULTILINE)
+        wx.Frame.__init__(self, parent, title=title, size=(300,150))
         self.Show(True)
 
+class myFrame(wx.Frame):
+    def __init__(self, parent, title):
+        wx.Frame.__init__(self, parent, title=title, size=(800,600))
+        self.control = wx.TextCtrl(self, style=wx.TE_MULTILINE)
+        self.CreateStatusBar()
+        
+        filemenu = wx.Menu()
+        
+        menuItem=filemenu.Append(wx.ID_ABOUT, "&About", " Information about this program")
+        self.Bind(wx.EVT_MENU, self.OnAbout, menuItem)
+        filemenu.AppendSeparator()
+        filemenu.Append(wx.ID_EXIT, "E&xit", " Terminate the program")
+        
+        menuBar = wx.MenuBar()
+        menuBar.Append(filemenu, "&File")
+        self.SetMenuBar(menuBar)
+        self.Show(True)
+        
+    def OnAbout(self, event):
+        about=aboutBox(self, "About Sample Editor")
+
 app = wx.App(False)
-frame = myFrame(None, "Small Editor")
+frame = myFrame(None, "Sample Editor")
 app.MainLoop()
 
