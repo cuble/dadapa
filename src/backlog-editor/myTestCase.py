dbaa53c87d35cf760379ad32c779175728b03789
Chen Gang-Cuble <fouryu@sina.com.cn>
2012-07-11 23:34:03 +0800

    doing mockPlugin, not complete yet

--- src/backlog-editor/myTestCase.py
+++ src/backlog-editor/myTestCase.py
@@ -11,21 +11,41 @@ class myTestCase(unittest.TestCase):
     def tearDown(self):
         pass
     
-class printPlugin(unittest.TestCase):
+def assert_equal(expected, real):
+    assert expected==real, '{0} != {1}'.format(expected, real)
+
+class printPlugin:
     def setUp(self):
         self.org_stdout = sys.stdout
         sys.stdout = StringIO()
 
     def tearDown(self):
         try:
-            self.assertEqual('', sys.stdout.getvalue())
+            assert_equal('', sys.stdout.getvalue())
         finally:
             sys.stdout = self.org_stdout
                 
     def check_print_result(self, expectedString):
-        self.assertEqual(expectedString, sys.stdout.getvalue())
+        assert_equal(expectedString, sys.stdout.getvalue())
         sys.stdout = StringIO()
 
+class UnexpectedCallError:
+    pass
+
+def mock_func(*param):
+    return []
+
+class mockPlugin:
+    def setUp(self):
+        self.mocked = False
+
+    def tearDown(self):
+        if self.mocked: 
+            raise UnexpectedCallError()
+    
+    def mock_function(self, function, param, expectedReturn):
+        dir = mock_func
+        self.mocked = True
 #-----------Test Case Part-------------------
 class testSut:
     pass
@@ -39,7 +59,9 @@ class myTestCaseTest(unittest.TestCase):
 
     def test_init_should_success(self):
         self.myTestCase.tearDown()
-
+#mock(wx.App, False, generalMock)
+#mockClass(mainWindow)
+#generalMock.MainLoop()
 
 class printPluginTest(unittest.TestCase):
     def setUp(self):
@@ -50,12 +72,15 @@ class printPluginTest(unittest.TestCase):
     def tearDown(self):
         sys.stdout = self.plugin.org_stdout
         
-    def test_not_operation_should_success(self):
+    def test_no_operation_should_success(self):
         self.plugin.tearDown()
 
     def test_fail_if_print_output_not_checked(self):
-        print 'Printed should be redirected for later checking'
-        self.assertRaises(AssertionError, self.plugin.tearDown)
+        testString = 'Printed should be redirected for later checking'
+        print testString
+        with self.assertRaises(AssertionError) as cm:
+            self.plugin.tearDown()
+        self.assertEqual('{0} != {1}\n'.format('',testString), cm.exception.message)
 
     def test_success_if_print_output_checked(self):
         testStr = 'Printed should be redirected for later checking' 
@@ -82,13 +107,28 @@ class printPluginTest(unittest.TestCase):
         self.plugin.tearDown()
         self.assertRaises(AttributeError, self.plugin.check_print_result, '')
         
-    def test_stdout_should_recover_stdout_even_teardown_failed(self):
+    def test_stdout_should_recover_even_teardown_failed(self):
         print '123'
         self.assertRaises(AssertionError, self.plugin.tearDown) 
         self.assertEqual(sys.stdout, self.plugin.org_stdout)
-#mock(wx.App, False, generalMock)
-#mockClass(mainWindow)
-#generalMock.MainLoop()
+
+class mockPluginTest(unittest.TestCase):
+    def setUp(self):
+        self.mock=mockPlugin()
+        self.mock.setUp()
+        
+    def test_success_if_no_operation(self):
+        self.mock.tearDown()
+        
+    def test_fail_if_not_call_mocked_function(self):
+        self.mock.mock_function(dir, '123',[])
+        self.assertRaises(UnexpectedCallError, self.mock.tearDown)
+    
+    @unittest.skip('not complete')
+    def test_success_if_mocked_function_called(self):
+        self.mock.mock_function(dir,'123',[])
+        self.assertEqual([], dir('123'))
+        self.mock.tearDown()
 
 if __name__=='__main__':
     unittest.main()
