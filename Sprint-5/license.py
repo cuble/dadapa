3d984bd8153f3a50daa36fc9ee530f750df9464d
Chen Gang-9T <fouryu@sina.com.cn>
2012-07-25 15:21:08 +0800

    add license information to my program

--- Sprint-5/license.py
+++ Sprint-5/license.py
@@ -18,6 +18,7 @@ license_txt='''
 '''
 
 import os
+import re
 
 def license_a_file(f,  result):
     tmpfileName=f.name+'.tmp'
@@ -26,29 +27,37 @@ def license_a_file(f,  result):
     lines = []
     for l in f:
         lines.append(l)
-    if not lines[0].startswith('#!'): lines.insert(0,'#! /usr/bin/env python')
+    if len(lines)==0 or not lines[0].startswith('#!'): lines.insert(0,'#! /usr/bin/env python\n')
     copyrightPresent = False
     for line in lines:
         if line.rfind('Copyright 2012 Chen Gang(fouryusteel@gmail.com)') != -1: copyrightPresent=True
     if not copyrightPresent:
         lines.insert(1,  license_txt)
-        result['Succeed'].append("{0} add license txt successful".format(f.name))
+        result['Succeed'].append(f.name)
     else:
-        result['Ignored'].append("{0} already contain license info".format(f.name))
+        result['Ignored'].append(f.name)
     tmpfile.writelines(lines)
     tmpfile.close()
     f.close()
     cmd='mv '+tmpfileName+' '+f.name
     os.system(cmd)
 
+def get_pattern(orgPat):
+    newPattern=orgPat.replace('*','[a-z,A-Z,-,_,0-9]*')
+    print newPattern
+    return newPattern + '$'
+
 if __name__=='__main__':
     import sys
-    if len(sys.argv) < 2: sys.argv[1] = '.'
-    if len(sys.argv) < 3: sys.argv[2] = '*.py'
+    print sys.argv
+    if len(sys.argv) < 2: sys.argv.append('.')
+    if len(sys.argv) < 3: sys.argv.append('*.py')
     files = os.listdir(sys.argv[1])
+    pattern = get_pattern(sys.argv[2])
     result = {'Succeed':[], 'Ignored':[]}
+    os.chdir(sys.argv[1])
     for name in files:
-        if name.match(sys.argv[2]):
+        if re.match(pattern, name):
             f = open(name)
             license_a_file(f,  result)
     print result
