de698accc802d74c2a4f27764576a94705a43ca8
Chen Gang-9T <fouryu@sina.com.cn>
2012-08-15 17:41:45 +0800

    designing the backlog editor

--- src/backlog-editor/BacklogEditor.mm
+++ src/backlog-editor/BacklogEditor.mm
@@ -1,59 +1,115 @@
 <map version="0.9.0">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1341822897885" ID="ID_1830842198" MODIFIED="1341903799083" TEXT="Backlog Editor">
-<node CREATED="1341823016451" ID="ID_1194765061" MODIFIED="1341903821507" POSITION="left" TEXT="Save the items structurely">
+<node CREATED="1341822897885" ID="ID_1830842198" MODIFIED="1345017903378" TEXT="Backlog Editor">
+<font NAME="SansSerif" SIZE="14"/>
+<node CREATED="1341823016451" ID="ID_1194765061" MODIFIED="1345017903378" POSITION="left" TEXT="Save the items structurely">
+<font NAME="SansSerif" SIZE="14"/>
 <icon BUILTIN="full-1"/>
 </node>
-<node CREATED="1341823156474" ID="ID_583574886" MODIFIED="1341909752165" POSITION="left" TEXT="Save the items safely">
+<node CREATED="1341823156474" ID="ID_583574886" MODIFIED="1345017903378" POSITION="left" TEXT="Save the items safely">
+<font NAME="SansSerif" SIZE="14"/>
 <icon BUILTIN="full-2"/>
 </node>
-<node CREATED="1341823844250" ID="ID_965933973" MODIFIED="1341909761583" POSITION="left" TEXT="Save the items versioning">
+<node CREATED="1341823844250" ID="ID_965933973" MODIFIED="1345017903377" POSITION="left" TEXT="Save the items versioning">
+<font NAME="SansSerif" SIZE="14"/>
 <icon BUILTIN="full-4"/>
 </node>
-<node CREATED="1341823272199" ID="ID_1540451869" MODIFIED="1341903905700" POSITION="right" TEXT="Extent the attributes of items">
+<node CREATED="1341823272199" ID="ID_1540451869" MODIFIED="1345017903376" POSITION="right" TEXT="Extent the attributes of items">
+<font NAME="SansSerif" SIZE="14"/>
 <icon BUILTIN="full-3"/>
-<node CREATED="1341826133507" ID="ID_1014010332" MODIFIED="1341826167197" TEXT="default attributes: done time, priority"/>
-<node CREATED="1341823397934" ID="ID_452080544" MODIFIED="1341823653223" TEXT="init attributes when create project"/>
-<node CREATED="1341823325589" ID="ID_537098681" MODIFIED="1341823333507" TEXT="extend the attributes">
-<node CREATED="1341826551797" ID="ID_1309299227" MODIFIED="1341826556705" TEXT="attribute name"/>
-<node CREATED="1341826557052" ID="ID_1574986315" MODIFIED="1341826568822" TEXT="type of attribute value"/>
-<node CREATED="1341826569304" ID="ID_1788622393" MODIFIED="1341826574009" TEXT="show attribute"/>
+<node CREATED="1341826133507" ID="ID_1014010332" MODIFIED="1345017903375" TEXT="default attributes: done time, priority">
+<font NAME="SansSerif" SIZE="14"/>
 </node>
-<node CREATED="1341823333951" ID="ID_983402033" MODIFIED="1341823341758" TEXT="remove attribute"/>
-<node CREATED="1341823342414" ID="ID_1386156311" MODIFIED="1341823346934" TEXT="modify attribute"/>
+<node CREATED="1341823397934" ID="ID_452080544" MODIFIED="1345017903375" TEXT="init attributes when create project">
+<font NAME="SansSerif" SIZE="14"/>
 </node>
-<node CREATED="1341823961349" ID="ID_280383477" MODIFIED="1341903876843" POSITION="right" TEXT="Edit items">
+<node CREATED="1341823325589" ID="ID_537098681" MODIFIED="1345017903374" TEXT="extend the attributes">
+<font NAME="SansSerif" SIZE="14"/>
+<node CREATED="1341826551797" ID="ID_1309299227" MODIFIED="1345017903374" TEXT="attribute name">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node CREATED="1341826557052" ID="ID_1574986315" MODIFIED="1345017903374" TEXT="type of attribute value">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node CREATED="1341826569304" ID="ID_1788622393" MODIFIED="1345017903374" TEXT="show attribute">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node CREATED="1341823333951" ID="ID_983402033" MODIFIED="1345017903373" TEXT="remove attribute">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node CREATED="1341823342414" ID="ID_1386156311" MODIFIED="1345017903373" TEXT="modify attribute">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node CREATED="1341823961349" ID="ID_280383477" MODIFIED="1345017903372" POSITION="right" TEXT="Edit items">
+<font NAME="SansSerif" SIZE="14"/>
 <icon BUILTIN="full-1"/>
-<node CREATED="1341824649811" ID="ID_430374829" MODIFIED="1341826762006" TEXT="add an item"/>
-<node CREATED="1341824656473" ID="ID_1335940667" MODIFIED="1341826766008" TEXT="remove an item"/>
-<node CREATED="1341823985861" ID="ID_58633237" MODIFIED="1341909727610" TEXT="Edit the content of an item"/>
-<node CREATED="1341827384336" ID="ID_784894792" MODIFIED="1341827391676" TEXT="Edit the attributes of an item"/>
-<node CREATED="1341823995668" ID="ID_999302005" MODIFIED="1341826775006" TEXT="move the position of an item"/>
-<node CREATED="1341824010582" ID="ID_444698876" MODIFIED="1341826779273" TEXT="add sub-item to an item"/>
-<node CREATED="1341824031255" ID="ID_746706473" MODIFIED="1341826782880" TEXT="delete sub-item of an item"/>
+<node CREATED="1341824649811" ID="ID_430374829" MODIFIED="1345017903372" TEXT="add an item">
+<font NAME="SansSerif" SIZE="14"/>
 </node>
-<node CREATED="1341822952523" ID="ID_498575165" MODIFIED="1341903842844" POSITION="left" TEXT="import exist items from txt files">
+<node CREATED="1341824656473" ID="ID_1335940667" MODIFIED="1345017903371" TEXT="remove an item">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node CREATED="1341823985861" ID="ID_58633237" MODIFIED="1345017903371" TEXT="Edit the content of an item">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node CREATED="1341827384336" ID="ID_784894792" MODIFIED="1345017903370" TEXT="Edit the attributes of an item">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node CREATED="1341823995668" ID="ID_999302005" MODIFIED="1345017903370" TEXT="move the position of an item">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node CREATED="1341824010582" ID="ID_444698876" MODIFIED="1345017903370" TEXT="add sub-item to an item">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node CREATED="1341824031255" ID="ID_746706473" MODIFIED="1345017903369" TEXT="delete sub-item of an item">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node CREATED="1341822952523" ID="ID_498575165" MODIFIED="1345017903368" POSITION="left" TEXT="import exist items from txt files">
+<font NAME="SansSerif" SIZE="14"/>
 <icon BUILTIN="full-1"/>
-<node CREATED="1341826503843" ID="ID_602764525" MODIFIED="1341826508372" TEXT="structure items"/>
-<node CREATED="1341826508730" ID="ID_341831453" MODIFIED="1341826520220" TEXT="init done time for the item"/>
+<node CREATED="1341826503843" ID="ID_602764525" MODIFIED="1345017903368" TEXT="structure items">
+<font NAME="SansSerif" SIZE="14"/>
 </node>
-<node CREATED="1341824050100" ID="ID_1634334258" MODIFIED="1341903849077" POSITION="left" TEXT="open a exist project">
+<node CREATED="1341826508730" ID="ID_341831453" MODIFIED="1345017903368" TEXT="init done time for the item">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node CREATED="1341824050100" ID="ID_1634334258" MODIFIED="1345017903368" POSITION="left" TEXT="open a exist project">
+<font NAME="SansSerif" SIZE="14"/>
 <icon BUILTIN="full-1"/>
 </node>
-<node CREATED="1341824131254" ID="ID_270992511" MODIFIED="1341903891923" POSITION="right" TEXT="sprint planning and review support">
+<node CREATED="1341824131254" ID="ID_270992511" MODIFIED="1345017903367" POSITION="right" TEXT="sprint planning and review support">
+<font NAME="SansSerif" SIZE="14"/>
 <icon BUILTIN="full-2"/>
-<node CREATED="1341824299266" ID="ID_1013772415" MODIFIED="1341824306994" TEXT="sprint grooming"/>
-<node CREATED="1341824307460" ID="ID_1138874770" MODIFIED="1341824311923" TEXT="sprint planning"/>
-<node CREATED="1341824312308" ID="ID_1323440913" MODIFIED="1341824317271" TEXT="sprint review"/>
+<node CREATED="1341824299266" ID="ID_1013772415" MODIFIED="1345017903367" TEXT="sprint grooming">
+<font NAME="SansSerif" SIZE="14"/>
 </node>
-<node CREATED="1341824686646" ID="ID_396936821" MODIFIED="1341903910900" POSITION="left" TEXT="nevigate in versions">
+<node CREATED="1341824307460" ID="ID_1138874770" MODIFIED="1345017903367" TEXT="sprint planning">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node CREATED="1341824312308" ID="ID_1323440913" MODIFIED="1345017903366" TEXT="sprint review">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node CREATED="1341824686646" ID="ID_396936821" MODIFIED="1345017903366" POSITION="left" TEXT="nevigate in versions">
+<font NAME="SansSerif" SIZE="14"/>
 <icon BUILTIN="full-4"/>
 </node>
-<node CREATED="1341826687644" ID="ID_1081843368" MODIFIED="1341903864748" POSITION="left" TEXT="show backlog items">
+<node CREATED="1341826687644" ID="ID_1081843368" MODIFIED="1345017903365" POSITION="left" TEXT="show backlog items">
+<font NAME="SansSerif" SIZE="14"/>
 <icon BUILTIN="full-1"/>
-<node CREATED="1341827144952" ID="ID_1138383462" MODIFIED="1341827153652" TEXT="show done and undone item sepratedly"/>
-<node CREATED="1341827153965" ID="ID_153373592" MODIFIED="1341827161111" TEXT="show high priority first"/>
-<node CREATED="1341827162829" ID="ID_592901921" MODIFIED="1341827193250" TEXT="sort items by name is also possible"/>
+<node CREATED="1341827144952" ID="ID_1138383462" MODIFIED="1345017903363" TEXT="show done and undone item sepratedly">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node CREATED="1341827153965" ID="ID_153373592" MODIFIED="1345017903357" TEXT="show high priority first">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node CREATED="1341827162829" ID="ID_592901921" MODIFIED="1345017903350" TEXT="sort items by name is also possible">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
 </node>
 </node>
 </map>
---
+++ src/backlog-editor/dsk module.mm
@@ -0,0 +1,4 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1345022350241" ID="ID_428773868" MODIFIED="1345022381276" TEXT="dsk module"/>
+</map>
