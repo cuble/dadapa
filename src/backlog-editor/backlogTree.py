47ac8a57dda61fa24ac5ede0f70e03bb062deaea
Chen Gang-9T <fouryu@sina.com.cn>
2012-08-17 17:58:18 +0800

    continue backlogTree test

--- src/backlog-editor/backlogTree.py
+++ src/backlog-editor/backlogTree.py
@@ -1,23 +1,40 @@
-def my_open(path, flag):
+def my_open(path, flag='r'):
     return open(path, flag)
+
+def my_close(f):
+    return f.close()
 
 class backlogTree:
     defaultAttr = {'priority': 1}
-    def __init__(self, content='', subTree=[], attribute={}):
+    def __init__(self, content='', attribute=None, subTree=None):
         self._content = content
-        self._subTree = subTree
         self._attribute = attribute
         if not attribute: self._attribute = backlogTree.defaultAttr
+        self._subTree = subTree
+        if not subTree: self._subTree = []
         
     def init_from_file(self, fileName):
         f=my_open(fileName)
         if not self._content: self._content = fileName
         for item in f:
             item = item.strip()
-            self._subTree.append(backlogTree(item, [], self._attribute))
+            self._subTree.append(backlogTree(item, self._attribute))
             
     def __eq__(self, other):
         if self._content != other._content: return False
         if self._attribute != other._attribute: return False
         if self._subTree != other._subTree: return False
         return True
+    
+#    def __ne__(self, other):
+#        return not self.__eq__(other)
+    
+    #for checking test result convinient
+    def __repr__(self):
+        repStr = repr(self._content)+'\n'
+        repStr += repr(self._attribute) + '\n'
+        repStr += '[\n'
+        for subTree in self._subTree:
+            repStr += '    ' + repr(subTree) + ',\n'
+        repStr += ']\n'  
+        return repStr
--- src/backlog-editor/testBacklogTree.py
+++ src/backlog-editor/testBacklogTree.py
@@ -1,48 +1,102 @@
-import myTestCase
+import myTestCase as mtc
 import backlogTree
 
-class testBacklogTree(myTestCase.myTestCase):
+class testFile:
+    def close(self):
+        pass
+
+class ownInterfaceTest(mtc.myTestCase):
+    def test_my_open_is_wrap_to_open(self):
+        fileName = 'another file'
+        flag = 'w+'
+        content = ['contents\n']
+        self.mock_function(open).with_param(fileName, flag).and_return(content)
+        self.assertEqual(content, backlogTree.my_open(fileName, flag))
+
+    def test_my_open_is_wrap_to_open_with_default_flag_r(self):
+        self.mock_function(open).with_param('another file', 'r').and_return([])
+        self.assertEqual([], backlogTree.my_open('another file'))
+
+    def test_my_close_is_wrap_to_close(self):
+        checkStr = 'for checking only'
+        f = testFile()
+        self.mock_function(f.close).and_return(checkStr)
+        self.assertEqual(checkStr, backlogTree.my_close(f))
+
+class backlogTreeTest(mtc.myTestCase):
+    class fileTest:
+        def __init__(self, fileName, fileContent, subTree):
+            self.name = fileName
+            self.content = fileContent
+            self.subTree = subTree
+            
+    file_test_data=[
+('two first level subtree', 
+['sub content 1\n', 
+           'sub content 2\n'],
+[backlogTree.backlogTree('sub content 1'), backlogTree.backlogTree('sub content 2')]),
+()]
+    
     def my_setup(self):
         pass
     
     def my_teardown(self):
         pass
+    
+    def _check_backlog_tree_content(self, bt, content, attr, subTree):
+        self.assertEqual(bt._content, content)
+        self.assertEqual(bt._attribute, attr)
+        self.assertEqual(bt._subTree, subTree)
 
     def test_create_blank_tree(self):
         bt = backlogTree.backlogTree()
-        self.assertEqual(bt._content, '')
-        self.assertEqual(bt._subTree, [])
-        self.assertEqual(bt._attribute, backlogTree.backlogTree.defaultAttr)
+        self._check_backlog_tree_content(bt, '', backlogTree.backlogTree.defaultAttr, [])
         
     def test_create_one_node_tree(self):
-        bt = backlogTree.backlogTree('root node', [], {'priority':2})
-        self.assertEqual(bt._content, 'root node')
-        self.assertEqual(bt._subTree, [])
-        self.assertEqual(bt._attribute, {'priority':2})
+        content = 'root node'
+        attribute = {'priority':2}
+        bt = backlogTree.backlogTree(content, attribute)
+        self._check_backlog_tree_content(bt, content, attribute, [])
         
-    def test_init_blank_tree_from_file(self):
+    def test_init_blank_tree_from_file_root_content_is_file_name(self):
         fileName = 'a backlog'
-        testFileContent = ['sub content 1\n', 'sub content 2\n']
+        content = ['sub content 1\n', 'sub content 2\n']
         defaultAttr = backlogTree.backlogTree.defaultAttr
-        self.mock_function(backlogTree.my_open).with_param(fileName).and_return(testFileContent)
+        self.mock_function(backlogTree.my_open).with_param(fileName).and_return(content)
+#        self.mock_function(backlogTree.my_close).with_param(content)
         bt = backlogTree.backlogTree()
         bt.init_from_file(fileName)
         self.assertEqual(fileName, bt._content)
         self.assertEqual(bt._attribute, defaultAttr)
-        self.assertEqual(bt._subTree, [backlogTree.backlogTree('sub content 1', [], defaultAttr),
-                                       backlogTree.backlogTree('sub content 2', [], defaultAttr)])
+        self.assertEqual(bt._subTree, [backlogTree.backlogTree('sub content 1'),
+                                       backlogTree.backlogTree('sub content 2')])
 
-    def test_init_one_node_tree_from_file(self):
+    def test_init_one_node_tree_from_file_file_name_is_ignored(self):
         fileName = 'a backlog'
-        testFileContent = ['sub content 1\n', 'sub content 2\n']
-        self.mock_function(backlogTree.my_open).with_param(fileName).and_return(testFileContent)
-        bt = backlogTree.backlogTree('root node', [], {'priority':2})
+        content = ['sub content 1\n', 'sub content 2\n']
+        self.mock_function(backlogTree.my_open).with_param(fileName).and_return(content)
+        bt = backlogTree.backlogTree('root node', {'priority':2}, [])
         bt.init_from_file(fileName)
         self.assertEqual('root node', bt._content)
         self.assertEqual({'priority':2}, bt._attribute)
-        self.assertEqual(bt._subTree, [backlogTree.backlogTree('sub content 1', [], {'priority':2}),
-                                       backlogTree.backlogTree('sub content 2', [], {'priority':2})])
+        self.assertEqual(bt._subTree, [backlogTree.backlogTree('sub content 1', {'priority':2}),
+                                       backlogTree.backlogTree('sub content 2', {'priority':2})])
         
+    def test_init_muti_level_tree_from_file(self):
+        fileName = '2 level backlog'
+        content = ['sub content 1\n', '  sub content 1: sub 1  \n', '  sub content 1: sub 2\n', 'sub content 2\n']
+        defaultAttr = backlogTree.backlogTree.defaultAttr
+        self.mock_function(backlogTree.my_open).with_param(fileName).and_return(content)
+        bt = backlogTree.backlogTree()
+        bt.init_from_file(fileName)
+        self.assertEqual(fileName, bt._content)
+        self.assertEqual(bt._attribute, defaultAttr)
+        sub_bt1_sub1 = backlogTree.backlogTree('sub content 1: sub 1')
+        sub_bt1_sub2 = backlogTree.backlogTree('sub content 1: sub 2')
+#        self.assertEqual(bt._subTree, [backlogTree.backlogTree('sub content 1', [sub_bt1_sub1, sub_bt1_sub2]),
+#                                       backlogTree.backlogTree('sub content 2')])
+
+
 if __name__ == '__main__':
     import unittest
     unittest.main()
